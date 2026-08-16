2abd718e0983a1575704a2d2c3b2d7f911bc06f2
Marco <marco@example.com>
2026-08-16 18:33:38 +0800

    Fix static view property compile error on WelcomeBanner class and remove temp clear.php

--- app/Filament/Widgets/WelcomeBanner.php
+++ app/Filament/Widgets/WelcomeBanner.php
@@ -6,7 +6,7 @@ use Filament\Widgets\Widget;
 
 class WelcomeBanner extends Widget
 {
-    protected static string $view = 'filament.widgets.welcome-banner';
+    protected string $view = 'filament.widgets.welcome-banner';
 
     protected static ?int $sort = 0; // Show on very top!
 
